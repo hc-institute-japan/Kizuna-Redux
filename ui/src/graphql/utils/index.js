c800cce27b03ea356f76079c8caf874341d45eed
Tatsuya Sato <tatsuya_sato@dlsu.edu.ph>
2020-09-07 11:36:10 +0900

    identified the onSignal bug

--- ui/src/graphql/utils/index.js
+++ ui/src/graphql/utils/index.js
@@ -12,20 +12,28 @@ export const getMyId = async (callZome) => {
   return myAddress;
 };
 
+const getAgentConfig = async (agentAddress, callAdmin) => {
+  const agentList = await callAdmin('admin/agent/list')({});
+  const agentName = agentList.find((a) => a.public_address === agentAddress);
+  return agentName;
+};
+
 export const initializeOrJoinP2PDNA = async (
   properties,
   callZome,
   callAdmin,
-  hcUprtcl
 ) => {
   console.log(properties);
-  const connection = await hcUprtcl();
+  // const connection = await hcUprtcl();
   const me = await getMyId(callZome);
-  const agentConfig = await connection.getAgentConfig(me);
+  console.log(me);
+  const agentConfig = await getAgentConfig(me, callAdmin);
+  console.log(agentConfig);
   const instanceId = getP2PInstanceId(
     properties.creator,
     properties.conversant
-  );
+    );
+    console.log(instanceId);
   // initialize properties
   const p2pProperties = {
     members: [properties.creator, properties.conversant],
@@ -35,15 +43,52 @@ export const initializeOrJoinP2PDNA = async (
 
   // clone DNA from template and initialize using properties
   try {
-    await connection.cloneDna(
-      agentConfig.id, // agent to 'host' the DNA
-      getP2PDnaId(properties.creator, properties.conversant), // DNA id
-      instanceId, // instance id
-      process.env.REACT_APP_DNA_PATH, // DNA address
-      p2pProperties, // properties
-      (interfaces) =>
-        interfaces.find((iface) => iface.id === "websocket-interface") // interface
-    );
+    // await connection.cloneDna(
+    //   agentConfig.id, // agent to 'host' the DNA
+    //   getP2PDnaId(properties.creator, properties.conversant), // DNA id
+    //   instanceId, // instance id
+    //   process.env.REACT_APP_DNA_PATH, // DNA address
+    //   p2pProperties, // properties
+    //   (interfaces) =>
+    //     interfaces.find((iface) => iface.id === "websocket-interface") // interface
+    // );
+    const newDnaId = getP2PDnaId(properties.creator, properties.conversant);
+    console.log(newDnaId);
+    const dnaPath = "/Users/tats/projects/Kizuna/dnas/p2pcomm/dist/p2pcomm.dna.json";
+    console.log(dnaPath);
+    const agentId = agentConfig.id;
+    console.log(agentId);
+    const dnaResult = await callAdmin('admin/dna/install_from_file')({
+      id: newDnaId,
+      path: dnaPath,
+      properties: p2pProperties,
+      copy: true,
+    });
+    console.log(dnaResult);
+    
+    const instanceResult = await callAdmin('admin/instance/add')({
+      id: instanceId,
+      agent_id: agentId,
+      dna_id: newDnaId,
+    });
+    console.log(instanceResult);
+    
+    const interfaceList = await callAdmin('admin/interface/list')();
+    // TODO: review this: what interface to pick?
+    console.log(interfaceList);
+    const iface = interfaceList.find((iface) => iface.id === "websocket-interface");
+    console.log(iface);
+    
+    // TATS: The bug is happening here for onSignal
+    const ifaceResult = callAdmin('admin/interface/add_instance')({
+      instance_id: instanceId,
+      interface_id: iface.id,
+    });
+    console.log(ifaceResult);
+    
+    await new Promise((resolve) => setTimeout(() => resolve(), 300));
+    const startResult = await callAdmin('admin/instance/start')({ id: instanceId });
+    console.log(startResult);
     return {
       id: instanceId,
       creator: properties.creator,
